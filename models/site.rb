933c3549264ee809c0e09f0af73cdd2f2236a63d
Kyle Drake <kyledrake@gmail.com>
2013-06-21 10:48:48 -0700

    fix the validation again

--- models/site.rb
+++ models/site.rb
@@ -74,9 +74,12 @@ class Site < Sequel::Model
 
     # Check for existing user
     
-    user = self.class.select(:username).filter(username: values[:username]).first
-    if !user.nil? || (!new? && user.id != values[:id])
-      errors.add :username, 'This username is already taken. Try using another one.'
+    user = self.class.select(:id, :username).filter(username: values[:username]).first
+    
+    if user
+      if user.id != values[:id]
+        errors.add :username, 'This username is already taken. Try using another one.'
+      end
     end
 
     if values[:password].nil? || (@password_length && @password_length < MINIMUM_PASSWORD_LENGTH)
