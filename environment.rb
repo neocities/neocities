ca6f890223f0aedb8510a33899e2fbd46a0a02ae
Kyle Drake <kyledrake@gmail.com>
2013-06-24 14:49:27 -0700

    prettier dev render

--- environment.rb
+++ environment.rb
@@ -51,3 +51,11 @@ Backburner.configure do |config|
   config.retry_delay = 200
   config.respond_timeout = 120
 end
+
+class Sinatra::Base
+  alias_method :render_original, :render
+  def render(engine, data, options = {}, locals = {}, &block)
+    options.merge!(pretty: self.class.development?) if engine == :slim && options[:pretty].nil?
+    render_original engine, data, options, locals, &block
+  end
+end
