d85482a6c4b67d0dafeb96ea4062272969c5c842
Kyle Drake <kyledrake@gmail.com>
2013-07-07 23:13:54 +0200

    updates to config

--- rainbows_config.rb
+++ rainbows_config.rb
@@ -1,24 +1,22 @@
 Rainbows! do
-  name = 'neocities'
-
   use :ThreadPool
 
-  client_max_body_size 12*1024*1024 # 12 Megabytes
+  client_max_body_size 100*1024*1024 # 100 Megabytes
 
-  worker_processes 6
+  worker_processes 8
   worker_connections 32
   timeout 10
 
-  listen "unix:tmp/#{name}.sock", :backlog => 2048
+  listen "unix:/var/run/neocities/neocities.sock", :backlog => 2048
 
-  pid "tmp/#{name}.pid"
-  stderr_path "tmp/#{name}.log"
-  stdout_path "tmp/#{name}.log"
+  pid "/var/run/neocities/neocities.pid"
+  stderr_path "/var/log/neocities/neocities.log"
+  stdout_path "/var/log/neocities/neocities.log"
 
   preload_app true
  
   before_fork do |server, worker|
-    old_pid = "tmp/#{name}.pid.oldbin"
+    old_pid = "/var/run/neocities/neocities.pid.oldbin"
     if File.exists?(old_pid) && server.pid != old_pid
       begin
         Process.kill("QUIT", File.read(old_pid).to_i)
