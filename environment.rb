3b0dd09b97bee3239229f9d1c8c56711cdd72035
Kyle Drake <kyle@kyledrake.net>
2014-04-23 15:07:00 -0700

    more copy fixes

--- environment.rb
+++ environment.rb
@@ -96,3 +96,13 @@ class Sinatra::Base
     render_original engine, data, options, locals, &block
   end
 end
+
+class Numeric
+  def roundup(nearest=10)
+    self % nearest == 0 ? self : self + nearest - (self % nearest)
+  end
+
+  def rounddown(nearest=10)
+    self % nearest == 0 ? self : self - (self % nearest)
+  end
+end
