5011d83436a4f760f30e4aade53b0d3ef3d23930
Victoria Wang <violasongoo@gmail.com>
2014-10-14 22:42:13 -0700

    Almost where I want the follow button to be, but not quite

--- public/js/news/site.js
+++ public/js/news/site.js
@@ -1,14 +1,11 @@
 var Site = {
   toggleFollow: function(siteId, csrfToken) {
     var link = $('a#followLink')
-    var span = $('a#followLink span')
     $.post('/site/'+siteId+'/toggle_follow', {csrf_token: csrfToken}, function(res) {
       if(res.result == "followed") {
-        span.text('Unfollow')
-        link.removeClass('follow')
+        link.addClass('is-following')        
       } else if(res.result == 'unfollowed') {
-        span.text('Follow')
-        link.addClass('follow')
+        link.removeClass('is-following')
       }
     })
   }
