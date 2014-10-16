8345b7aa04dc7b1df3a35b063d47631b5815b009
Kyle Drake <kyle@kyledrake.net>
2014-10-15 19:06:39 -0700

    update plans for live data and extra fun things

--- ext/numeric.rb
+++ ext/numeric.rb
@@ -1,5 +1,5 @@
 class Numeric
-  ONE_MEGABYTE = 1048576
+  ONE_MEGABYTE = 1000000
 
   def roundup(nearest=10)
     self % nearest == 0 ? self : self + nearest - (self % nearest)
@@ -9,9 +9,19 @@ class Numeric
     self/ONE_MEGABYTE.to_f
   end
 
-  def to_space_pretty
+  def to_bytes_pretty
     space = (self.to_f / ONE_MEGABYTE).round(2)
     space = space.to_i if space.denominator == 1
-    "#{space} MB"
+    if space >= 1000000
+      "#{space/1000000} TB"
+    elsif space >= 1000
+      "#{space/1000} GB"
+    else
+      "#{space} MB"
+    end
+  end
+
+  def to_space_pretty
+    to_bytes_pretty
   end
 end
