d2f46fdf662d5af6d9600fc5b04fc19259218321
Kyle Drake <kyle@kyledrake.net>
2014-10-24 18:16:00 -0700

    implement skeuocard and upgrade capability

--- app.rb
+++ app.rb
@@ -267,6 +267,42 @@ get '/plan/?' do
   erb :'plan/index'
 end
 
+post '/plan/update' do
+  require_login
+
+  DB.transaction do
+    customer = Stripe::Customer.create(
+      card: params[:stripe_token],
+      description: "#{parent_site.username} - #{parent_site.id}",
+      email: (current_site.email || parent_site.email),
+      plan: params[:plan_type]
+    )
+
+    parent_site.update(
+      stripe_customer_id: customer.id,
+      plan_ended: false,
+      plan_type: params[:plan_type]
+    )
+
+    if current_site.email || parent_site.email
+      EmailWorker.perform_async({
+        from: 'web@neocities.org',
+        reply_to: 'contact@neocities.org',
+        to: current_site.email || parent_site.email,
+        subject: "[Neocities] You've become a supporter!",
+        body: Tilt.new('./views/templates/email_subscription.erb', pretty: true).render(self, plan_name: Site::PLAN_FEATURES[params[:plan_type].to_sym][:name])
+      })
+    end
+  end
+
+  redirect '/plan/thanks'
+end
+
+get '/plan/thanks' do
+  require_login
+  erb :'plan/thanks'
+end
+
 =begin
 get '/plan/?' do
   @title = 'Supporter'
