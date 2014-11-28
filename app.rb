14d29b5bb529efd03e841abb20225623b6638625
Kyle Drake <kyle@kyledrake.net>
2014-11-27 17:32:44 -0800

    fix to sslsites

--- app.rb
+++ app.rb
@@ -1487,8 +1487,8 @@ get '/sysops/proxy/sslcerts.tar.gz' do
         server {
           listen 443 ssl;
           server_name #{site.domain} *.#{site.domain};
-          ssl_certificate sslcerts/certs/#{site.username}.crt;
-          ssl_certificate_key sslcerts/certs/#{site.username}.key;
+          ssl_certificate sslsites/certs/#{site.username}.crt;
+          ssl_certificate_key sslsites/certs/#{site.username}.key;
 
           location / {
             proxy_http_version 1.1;
