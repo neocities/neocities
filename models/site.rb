0a8b33f67a60275f399790a9eb842304b5dadcca
Kyle Drake <kyle@kyledrake.net>
2014-12-23 13:17:54 -0800

    update for special -> supporter

--- models/site.rb
+++ models/site.rb
@@ -914,7 +914,7 @@ class Site < Sequel::Model
 
   def plan_type
     return 'free' if owner.values[:plan_type].nil?
-    return 'supporter' if owner.values[:plan_type].match /^plan_/
+    return 'supporter' if owner.values[:plan_type].match /^plan_/ || owner.values[:plan_type] == 'special'
     owner.values[:plan_type]
   end
 
