b7fe2ab8eea69823cb0b9b1368c5085648beaaef
Kyle Drake <kyle@kyledrake.net>
2015-01-08 13:22:18 -0800

    fixes for missing site dir on dashboard

--- app/dashboard.rb
+++ app/dashboard.rb
@@ -3,3 +3,23 @@ get '/dashboard' do
   dashboard_init
   erb :'dashboard'
 end
+
+def dashboard_init
+  if params[:dir] && params[:dir][0] != '/'
+    params[:dir] = '/'+params[:dir]
+  end
+
+  if !File.directory?(current_site.files_path(params[:dir]))
+    if !File.directory?(current_site.files_path)
+      flash[:error] = 'Could not find your web site, please contact support.'
+      session[:id] = nil
+      redirect '/'
+    else
+      flash[:error] = 'Could not find the requested directory.'
+      redirect '/dashboard'
+    end
+  end
+
+  @dir = params[:dir]
+  @file_list = current_site.file_list @dir
+end
