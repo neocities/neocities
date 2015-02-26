b822d4345b42f10b07925ed8f0159d961812b974
Kyle Drake <kyle@kyledrake.net>
2015-02-26 11:17:40 -0800

    ability to view individual event #71:close

--- app/activity.rb
+++ app/activity.rb
@@ -1,6 +1,14 @@
 get '/activity' do
   #expires 7200, :public, :must_revalidate if self.class.production? # 2 hours
   params[:activity] = 'global' # FIXME this is a bad hack
-  @events = Event.global_dataset.all
+
+  global_dataset = Event.global_dataset
+
+  if params[:event_id]
+    global_dataset.where! Sequel.qualify(:events, :id) => params[:event_id]
+  end
+
+  @events = global_dataset.all
+
   erb :'activity'
 end
