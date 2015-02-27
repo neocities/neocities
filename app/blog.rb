314e67adcd2198c1881e1fd3376ea706dbf5706e
Kyle Drake <kyle@kyledrake.net>
2015-02-27 12:10:29 -0800

    a little bit faster cache recycle

--- app/blog.rb
+++ app/blog.rb
@@ -2,12 +2,12 @@ require 'net/http'
 require 'uri'
 
 get '/blog/?' do
-  expires 500, :public, :must_revalidate
+  expires 60, :public, :must_revalidate
   return Net::HTTP.get_response(URI('http://blog.neocities.org')).body
 end
 
 get '/blog/:article' do |article|
-  expires 500, :public, :must_revalidate
+  expires 60, :public, :must_revalidate
 
   attempted = false
 
