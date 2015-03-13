4e0ff8a7ca4fc405922d2763d8564524a7e5a39d
Kyle Drake <kyle@kyledrake.net>
2015-03-13 09:58:10 -0500

    send email for new customers, closes #103

--- app/stripe_webhook.rb
+++ app/stripe_webhook.rb
@@ -1,8 +1,15 @@
 post '/stripe_webhook' do
   event = JSON.parse request.body.read
   if event['type'] == 'customer.created'
-    username  = event['data']['object']['description']
+    username  = event['data']['object']['description'].split(' - ').first
     email     = event['data']['object']['email']
+
+    EmailWorker.perform_async({
+      from:    'web@neocities.org',
+      to:      'contact@neocities.org',
+      subject: "[Neocities] New customer: #{username}",
+      body:    "#{username}\n#{email}\n#{Site[username: username].uri}"
+    })
   end
   'ok'
 end
