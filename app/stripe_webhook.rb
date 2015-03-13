d085a99ddc07228a592e0ff6623153753ab761fb
Kyle Drake <kyle@kyledrake.net>
2015-03-13 11:27:39 -0500

    Add more catches for stripe events

--- app/stripe_webhook.rb
+++ app/stripe_webhook.rb
@@ -11,5 +11,33 @@ post '/stripe_webhook' do
       body:    "#{username}\n#{email}\n#{Site[username: username].uri}"
     })
   end
+
+  if event['type'] == 'charge.failed'
+    site_id = event['data']['object']['description'].split(' - ').last
+    site = Site[site_id]
+
+    EmailWorker.perform_async({
+      from:    'web@neocities.org',
+      to:      site.email,
+      subject: "[Neocities] There was an issue charging your card",
+      body:    Tilt.new('./views/templates/email/charge_failure.erb', pretty: true).render(self)
+    })
+  end
+
+  if event['type'] == 'customer.subscription.deleted'
+    site_id = event['data']['object']['description'].split(' - ').last
+    site = Site[site_id]
+    site.stripe_subscription_id = nil
+    site.plan_type = nil
+    site.save_changes validate: false
+
+    EmailWorker.perform_async({
+      from:    'web@neocities.org',
+      to:      site.email,
+      subject: "[Neocities] Supporter plan has ended",
+      body:    Tilt.new('./views/templates/email/supporter_ended.erb', pretty: true).render(self)
+    })
+  end
+
   'ok'
 end
