334a9c019a987586e82c35d633bd3f623361fac0
Kyle Drake <kyle@kyledrake.net>
2015-06-17 14:20:43 -0700

    Add url hash index for stat_referrers

---
+++ migrations/067_add_missing_stat_indexes.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up {
+    DB['create index stat_referrers_hash_multi on stat_referrers (site_id, md5(url))'].first
+    DB.add_index :stat_locations, :site_id
+    DB.add_index :stat_paths,     :site_id
+  }
+
+  down {
+    DB['drop index stat_referrers_hash_multi'].first
+    DB.drop_index :stat_locations, :site_id
+    DB.drop_index :stat_paths,     :site_id
+  }
+end
