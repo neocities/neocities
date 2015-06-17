c9b4393dce76729fed4b7a0fd504a975f33a0bf4
Kyle Drake <kyle@kyledrake.net>
2015-06-17 14:31:40 -0700

    Fix for iterator

--- migrations/069_add_stat_created_indexes.rb
+++ migrations/069_add_stat_created_indexes.rb
@@ -1,12 +1,12 @@
 Sequel.migration do
   up {
-    %i{stat_referrers stat_locations stat_paths} do |t|
+    %i{stat_referrers stat_locations stat_paths}.each do |t|
       DB.add_index t, :created_at
     end
   }
 
   down {
-    %i{stat_referrers stat_locations stat_paths} do |t|
+    %i{stat_referrers stat_locations stat_paths}.each do |t|
       DB.drop_index t, :created_at
     end
   }
