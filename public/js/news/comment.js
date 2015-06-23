e50239ee39ed26badbce96d96faad9e01fe7d8ca
Kyle Drake <kyle@kyledrake.net>
2015-06-23 11:22:27 -0700

    fix for comment issue

--- public/js/news/comment.js
+++ public/js/news/comment.js
@@ -3,16 +3,13 @@ var Comment = {
     var form = $(form)
     var comment = form.find('[name="comment"]').val()
     form.remove()
-
     $.post('/event/'+eventId+'/comment', {csrf_token: csrfToken, message: comment}, function(res) {
-      console.log(res)
       location.reload()
     })
   },
 
   delete: function(commentId, csrfToken) {
     $.post('/comment/'+commentId+'/delete', {csrf_token: csrfToken}, function(res) {
-      console.log(res)
       location.reload()
     })
   },
