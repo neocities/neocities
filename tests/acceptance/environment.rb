ad89bf418758755ef44c738420f46de129fc1712
Kyle Drake <kyle@kyledrake.net>
2016-01-21 17:14:45 -0800

    Check in working Gemfile.lock due to dep problems

--- tests/acceptance/environment.rb
+++ tests/acceptance/environment.rb
@@ -1,9 +1,12 @@
 require_relative '../environment'
 
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+require 'rack_session_access/capybara'
+
 Capybara.app = Sinatra::Application
 
 def teardown
   Capybara.reset_sessions!
 end
-
-Capybara.default_wait_time = 5
