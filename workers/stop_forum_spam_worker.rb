65008f4ac409c57b41a370332ceb1a69017e5c81
Kyle Drake <kyle@kyledrake.net>
2017-01-15 15:07:58 -0800

    pass stopforumspam as formencoded

--- workers/stop_forum_spam_worker.rb
+++ workers/stop_forum_spam_worker.rb
@@ -4,7 +4,7 @@ class StopForumSpamWorker
 
   def perform(opts)
     opts.merge! api_key: $config['stop_forum_spam_api_key']
-    res = HTTP.post 'https://stopforumspam.com/add', opts
+    res = HTTP.post 'https://stopforumspam.com/add', form: opts
     puts res.inspect
   end
 end
