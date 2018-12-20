706c9a19ece2dfe07022c42b749049e50a65d34d
Kyle Drake <kyle@kyledrake.net>
2018-12-20 05:07:25 +0000

    Move puma back to multi-threaded mode

--- puma_config.rb
+++ puma_config.rb
@@ -1,13 +1,12 @@
 require 'facter'
 
-threads 1, 1
+#threads 1, 1
 environment 'production'
 #daemonize
 pidfile '/var/run/neocities/neocities.pid'
-stdout_redirect '/var/log/neocities/neocities.log', '/var/log/neocities/neocities-errors.log', true
+stdout_redirect '/var/log/neocities/neocities.stdout.log', '/var/log/neocities/neocities.stderr.log', true
 quiet
 workers Facter.value('processors')['count']
-worker_timeout 600
 preload_app!
 on_worker_boot { DB.disconnect }
 bind 'unix:/var/run/neocities/neocities.sock?backlog=2048'
