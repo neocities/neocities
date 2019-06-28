c5d019494659411f230b7644de72d7511ae2a49d
Kyle Drake <kyle@kyledrake.net>
2019-06-28 15:08:22 -0700

    fix for profile comment editing

--- public/js/news/profile_comment.js
+++ public/js/news/profile_comment.js
@@ -1,24 +1,24 @@
 var ProfileComment = {
   displayEditor: function(eventId) {
-    var commentDiv = $('#event_'+eventId+' div.title div.comment')
+    var commentDiv = $('#event_'+eventId+' div.content')
     var eventActions = $('#event_'+eventId+'_actions')
-    
+
     eventActions.find('a#editLink').css('display', 'none')
-    
+
     commentDiv.html(Template.template($('#comment-edit-template').html(), {eventId: eventId, content: commentDiv.text()}))
     $('#event_'+eventId+' div.title div.comment').text()
   },
-  
+
   cancelEditor: function(eventId) {
     var eventActions = $('#event_'+eventId+'_actions')
-    var commentDiv = $('#event_'+eventId+' div.title div.comment')
+    var commentDiv = $('#event_'+eventId+' div.content')
     eventActions.find('a#editLink').css('display', 'inline')
     commentDiv.text(commentDiv.find('textarea').text())
   },
-  
+
   update: function(eventId, csrfToken) {
     var eventActions = $('#event_'+eventId+'_actions')
-    var commentDiv = $('#event_'+eventId+' div.title div.comment')
+    var commentDiv = $('#event_'+eventId+' div.content')
     var self = this
     console.log(commentDiv.find('textarea').val())
     $.post('/event/'+eventId+'/update_profile_comment', {
