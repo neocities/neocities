4533214d71d6bedd5dd33324d1eba23593c2c6b6
Kyle Drake <kyle@kyledrake.net>
2020-05-27 04:02:45 +0000

    thumbnailworker: reduce memory issues

--- workers/thumbnail_worker.rb
+++ workers/thumbnail_worker.rb
@@ -2,17 +2,30 @@ require 'rmagick'
 
 class ThumbnailWorker
   THUMBNAILS_PATH = Site::THUMBNAILS_ROOT
+  MAXIMUM_IMAGE_SIZE = 2_000_000 # 2MB
+
   include Sidekiq::Worker
   sidekiq_options queue: :thumbnails, retry: 3, backtrace: true
 
   def perform(username, path)
     site = Site[username: username]
+    return if site.nil?
+
+    site_file_path = site.files_path(path)
+    return unless File.exist?(site_file_path)
+
+    # Large images jam up ImageMagick and eat a ton of memory, so we skip for now.
+    return if File.size(site_file_path) > MAXIMUM_IMAGE_SIZE
 
     img_list = Magick::ImageList.new
 
     begin
-      img_list.from_blob File.read(site.files_path(path))
+      img_list.from_blob File.read(site_file_path)
     rescue Errno::ENOENT => e # Not found, skip
+      return
+    rescue Magick::ImageMagickError => e
+      GC.start full_mark: true, immediate_sweep: true
+      puts "thumbnail fail: #{site_file_path} #{e.inspect}"
       return
     end
 
@@ -33,7 +46,11 @@ class ThumbnailWorker
       resimg.write(full_thumbnail_path) {
         self.quality = 75
       }
+      resimg.destroy!
       #$image_optim.optimize_image! full_thumbnail_path
     end
+
+    img.destroy!
+    GC.start full_mark: true, immediate_sweep: true
   end
 end
