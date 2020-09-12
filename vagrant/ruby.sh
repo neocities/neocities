3012b0c2393810aee9479e9bbd6ecdee91b257d1
joppiesaus <job@function1.nl>
2020-09-12 18:14:38 +0200

    Fix vagrant not working
    Some Gem needed Ruby 2.5, so install that instead over 2.4

--- vagrant/ruby.sh
+++ vagrant/ruby.sh
@@ -3,5 +3,5 @@
 apt-get -y install python-software-properties
 apt-add-repository -y ppa:brightbox/ruby-ng
 apt-get -y update
-apt-get -y install ruby2.4 ruby2.4-dev
+apt-get -y install ruby2.5 ruby2.5-dev
 gem install bundler --no-rdoc --no-ri
