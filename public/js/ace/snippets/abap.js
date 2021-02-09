e9ff2ea633d4d3aeabd17341730bfadd62219d82
Kyle Drake <kyle@kyledrake.net>
2021-02-08 17:59:39 -0700

    update ace editor to 1.4.12

--- public/js/ace/snippets/abap.js
+++ public/js/ace/snippets/abap.js
@@ -1,5 +1,5 @@
-define("ace/snippets/abap",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="abap"});                (function() {
-                    window.require(["ace/snippets/abap"], function(m) {
+;                (function() {
+                    ace.require(["ace/snippets/abap"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
                         }
--- public/js/ace/snippets/cirru.js
+++ public/js/ace/snippets/cirru.js
@@ -1,5 +1,5 @@
-define("ace/snippets/cirru",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="cirru"});                (function() {
-                    window.require(["ace/snippets/cirru"], function(m) {
+;                (function() {
+                    ace.require(["ace/snippets/cirru"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
                         }
