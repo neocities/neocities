f387059c6687f21f5c4109a95d97e3a6ab016b80
Kyle Drake <kyle@kyledrake.net>
2024-01-04 18:02:53 +0000

    puma: use phased restart instead of preload

--- puma_config.rb
+++ puma_config.rb
@@ -7,7 +7,8 @@ pidfile '/var/run/neocities/neocities.pid'
 stdout_redirect '/var/log/neocities/neocities.stdout.log', '/var/log/neocities/neocities.stderr.log', true
 quiet
 workers Facter.value('processors')['count']
-preload_app!
+#preload_app!
+prune_bundler
 on_worker_boot { DB.disconnect }
 bind 'unix:/var/run/neocities/neocities.sock?backlog=2048'
 supported_http_methods Puma::Const::IANA_HTTP_METHODS
