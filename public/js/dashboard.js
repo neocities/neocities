1cb99d770645c3e7d75d54b0d0410d301876dfc8
Kyle Drake <kyle@kyledrake.net>
2024-03-11 10:40:48 -0500

    last minute fixes and cleanups

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,99 +1,6 @@
 if(localStorage && localStorage.getItem('viewType') == 'list')
   $('#filesDisplay').addClass('list-view')
 
-function uploadFileFromButton() {
-  var form = $('#uploadFilesButtonForm')[0];
-  var dirValue = $('#dir').val();
-  var formData = new FormData();
-
-  // Append other form data
-  formData.append('csrf_token', $(form).find('input[name="csrf_token"]').val());
-  formData.append('from_button', $(form).find('input[name="from_button"]').val());
-  formData.append('dir', dirValue);
-
-  uploadFilesCount = 0
-  // Append files with modified filenames
-  $.each($('#uploadFiles')[0].files, function(i, file) {
-    var modifiedFileName = dirValue + '/' + file.name;
-    formData.append(modifiedFileName, file);
-    uploadFilesCount++;
-  });
-
-  alertClear();
-
-  $.ajax({
-    url: '/api/upload',
-    type: 'POST',
-    data: formData,
-    contentType: false, // This is required for FormData
-    processData: false, // This is required for FormData
-    success: function(data) {
-      alertType('success');
-      alertAdd(uploadFilesCount+' files uploaded successfully.');
-      reloadDashboardFiles();
-    },
-    error: function(xhr, status, error) {
-      var responseBody = JSON.parse(xhr.responseText);
-      alertType('error');
-      alertAdd(responseBody.message);
-      reloadDashboardFiles();
-    }
-  });
-}
-
-$('#uploadFiles').change(function() {
-  $('#uploadFilesButtonForm').submit();
-});
-
-var uploadForm = $('#uploadFilesButtonForm')[0];
-var deleteForm = $('#deleteFilenameForm')[0];
-
-function moveFileToFolder(event) {
-  var link = event.dataTransfer.getData("Text");
-  if(link) link = link.trim();
-  if(!link || link.startsWith('https://neocities.org/dashboard')) return;
-  event.preventDefault();
-  var name = link.split('.neocities.org/').slice(1).join('.neocities.org/');
-  var oReq = new XMLHttpRequest();
-  oReq.open("GET", "/site_files/download/" + name, true);
-  oReq.responseType = "arraybuffer";
-
-  $('#movingOverlay').css('display', 'block')
-
-  oReq.onload = function() {
-    var newFile = new File([oReq.response], name);
-    var dataTransfer = new DataTransfer();
-    var currentFolder = new URL(location.href).searchParams.get('dir');
-    if(!currentFolder) currentFolder = '';
-    else currentFolder = currentFolder + '/';
-
-    dataTransfer.items.add(newFile);
-    $('#uploadFilesButtonForm > input[name="dir"]')[0].value = currentFolder + event.target.parentElement.parentElement.getElementsByClassName('title')[0].innerText.trim();
-    $('#uploadFiles')[0].files = dataTransfer.files;
-    $.ajax({
-      type: uploadForm.method,
-      url: uploadForm.action,
-      data: new FormData(uploadForm),
-      processData: false,
-      contentType: false,
-      success: function() {
-        let csrf = $('#uploadFilesButtonForm > input[name="csrf_token"]')[0].value;
-        var dReq = new XMLHttpRequest();
-        dReq.open(deleteForm.method, deleteForm.action, true);
-        dReq.onload = function() {
-          location.reload()
-        }
-        dReq.setRequestHeader("content-type", 'application/x-www-form-urlencoded');
-        dReq.send("csrf_token=" + encodeURIComponent(csrf) + "&filename=" + name.replace(/\s/g, '+'));
-      },
-      error: function() {
-        location.reload()
-      }
-    });
-  };
-  oReq.send();
-}
-
 function confirmFileRename(path) {
   $('#renamePathInput').val(path);
   $('#renameNewPathInput').val(path);
@@ -183,8 +90,7 @@ function reInitDashboardFiles() {
     uploadMultiple: false,
     parallelUploads: 1,
     maxFilesize: 104857600, // 100MB
-    clickable: false,
-
+    clickable: document.getElementById('uploadButton'),
     init: function() {
       this.on("processing", function(file) {
         var dir = $('#uploads input[name="dir"]').val();
@@ -233,10 +139,15 @@ function reInitDashboardFiles() {
       });
     }
   });
+
+  document.getElementById('uploadButton').addEventListener('click', function(event) {
+      event.preventDefault();
+  });
 }
 
 function reloadDashboardFiles() {
-  $.get('/dashboard/files?dir='+encodeURIComponent($("#dir").val()), function(data) {
+  var dir = $('#uploads input[name="dir"]').val();
+  $.get('/dashboard/files?dir='+encodeURIComponent(dir), function(data) {
     $('#filesDisplay').html(data);
     reInitDashboardFiles();
   });
@@ -244,6 +155,3 @@ function reloadDashboardFiles() {
 
 // for first time load
 reInitDashboardFiles();
-
-
-
