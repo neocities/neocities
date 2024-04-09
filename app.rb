be6c4252ff1d070288beb4eeac585975527a6315
Kyle Drake <kyle@kyledrake.net>
2024-04-09 18:06:25 -0500

    no error emails, using airbrake

--- app.rb
+++ app.rb
@@ -105,6 +105,7 @@ not_found do
 end
 
 error do
+=begin
   EmailWorker.perform_async({
     from: 'web@neocities.org',
     to: 'errors@neocities.org',
@@ -112,6 +113,7 @@ error do
     body: erb(:'templates/email/error', layout: false),
     no_footer: true
   })
+=end
 
   if @api
     api_error 500, 'server_error', 'there has been an unknown server error, please try again later'
