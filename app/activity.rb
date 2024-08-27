6ced50a88ddde1efda53e03e502e8d88e061a4d1
Kyle Drake <kyle@kyledrake.net>
2024-08-26 22:54:45 -0500

    disable activity cache

--- app/activity.rb
+++ app/activity.rb
@@ -1,5 +1,5 @@
 get '/activity' do
-  expires 7200, :public, :must_revalidate if self.class.production? # 2 hours
+  #expires 7200, :public, :must_revalidate if self.class.production? # 2 hours
 
   @page = params[:page] || 1
   @pagination_dataset = Event.global_dataset.paginate(@page.to_i, Event::GLOBAL_PAGINATION_LENGTH)
