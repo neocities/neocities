724ee54658a33b4fd09cee016f9472dba7f822ff
Kyle Drake <kyle@kyledrake.net>
2024-08-29 17:09:45 -0500

    fix for comment issue

--- app/event.rb
+++ app/event.rb
@@ -2,8 +2,8 @@ post '/event/:event_id/toggle_like' do |event_id|
   require_login
   content_type :json
   event = Event[id: event_id]
-  return 403 if event.site.is_blocking?(current_site)
-  return 403 if event.actioning_site.is_blocking?(current_site)
+  return 403 if event.site && event.site.is_blocking?(current_site)
+  return 403 if event.actioning_site && event.actioning_site.is_blocking?(current_site)
   liked_response = event.toggle_site_like(current_site) ? 'liked' : 'unliked'
   {result: liked_response, event_like_count: event.likes_dataset.count, liking_site_names: event.liking_site_usernames}.to_json
 end
@@ -13,7 +13,8 @@ post '/event/:event_id/comment' do |event_id|
   content_type :json
   event = Event[id: event_id]
 
-  return 403 if event.actioning_site.is_blocking?(current_site)
+  return 403 if event.site && event.site.is_blocking?(current_site)
+  return 403 if event.actioning_site && event.actioning_site.is_blocking?(current_site)
 
   site = event.site
 
