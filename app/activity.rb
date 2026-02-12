026c5b6b0faf61c088fb344bd6950ab6ba71aec3
Kyle Drake <kyle@kyledrake.net>
2026-02-12 17:32:40 -0600

    cache activities display and cap to 100 pages

--- app/activity.rb
+++ app/activity.rb
@@ -1,5 +1,6 @@
 get '/activity' do
-  @page = params[:page] || 1
+  @page = params[:page].to_i
+  @page = 1 if @page < 1
   @description = 'See recent activity from Neocities websites.'
 
   halt 404 if params[:event_id]
@@ -49,7 +50,28 @@ get '/activity' do
     )
   end
 
-  @pagination_dataset = ds.paginate @page.to_i, Event::GLOBAL_PAGINATION_LENGTH
+  record_count = nil
+  if current_site.nil?
+    cache_key = if params[:tag]
+      ['activity_public_tag_record_count', params[:tag].to_s]
+    else
+      :activity_public_record_count
+    end
+
+    if SimpleCache.expired?(cache_key)
+      record_count = SimpleCache.store(cache_key, ds.count, 10.minutes)
+    else
+      record_count = SimpleCache.get(cache_key)
+    end
+
+    max_record_count = Event::GLOBAL_PAGINATION_LENGTH * 100
+    record_count = [record_count, max_record_count].min
+    max_page = (record_count / Event::GLOBAL_PAGINATION_LENGTH.to_f).ceil
+    max_page = 1 if max_page < 1
+    halt 404 if @page > max_page
+  end
+
+  @pagination_dataset = ds.paginate(@page, Event::GLOBAL_PAGINATION_LENGTH, record_count)
   @events = @pagination_dataset.all
 
   erb :'activity'
