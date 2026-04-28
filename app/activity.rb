b9549bac1cc2d2338d2bcd92b1b471498e63ec0b
Kyle Drake <kyle@kyledrake.net>
2026-04-28 00:51:24 -0500

    check tag before activity cache, improve simplecache

--- app/activity.rb
+++ app/activity.rb
@@ -5,37 +5,57 @@ get '/activity' do
 
   halt 404 if params[:event_id]
 
+  tag = nil
   if params[:tag]
-    @description = "See recent activity from Neocities websites tagged #{params[:tag]}."
-    query1 = Event
-      .join(:sites, id: :site_id)
-      .join(:sites_tags, site_id: :id)
-      .join(:tags, id: :tag_id)
-      .where(tags__name: params[:tag])
-      .where(events__is_deleted: false, sites__is_deleted: false)
-      .where{sites__score > Event::ACTIVITY_TAG_SCORE_LIMIT}
-      .where(sites__is_nsfw: false)
-      .where(follow_id: nil)
-      .select_all(:events)
-
-    query2 = Event
-      .join(:sites, id: :actioning_site_id)
-      .join(:sites_tags, site_id: :id)
-      .join(:tags, id: :tag_id)
-      .where(tags__name: params[:tag])
-      .where(events__is_deleted: false, sites__is_deleted: false)
-      .where{sites__score > Event::ACTIVITY_TAG_SCORE_LIMIT}
-      .where(sites__is_nsfw: false)
-      .where(follow_id: nil)
-      .select_all(:events)
-
-    if current_site
-      blocking_site_ids = current_site.blocking_site_ids
-      query1 = query1.where(Sequel.|({events__site_id: nil}, ~{events__site_id: blocking_site_ids})).where(Sequel.|({events__actioning_site_id: nil}, ~{events__actioning_site_id: blocking_site_ids}))
-      query2 = query2.where(Sequel.|({events__site_id: nil}, ~{events__site_id: blocking_site_ids})).where(Sequel.|({events__actioning_site_id: nil}, ~{events__actioning_site_id: blocking_site_ids}))
+    begin
+      tag = Tag.clean_name(params[:tag].to_s)
+      if tag.empty? || tag.length > Tag::NAME_LENGTH_MAX || tag.match?(Tag::INVALID_TAG_REGEX) || tag.match?(/\s/)
+        params.delete 'tag'
+        tag = nil
+      else
+        params[:tag] = tag
+      end
+    rescue Encoding::CompatibilityError, ArgumentError
+      params.delete 'tag'
     end
+  end
 
-    ds = query1.union(query2, all: false).order(Sequel.desc(:created_at))
+  tag_record = nil
+  if params[:tag]
+    tag_record = Tag.select(:id).where(name: params[:tag]).first
+    @description = "See recent activity from Neocities websites tagged #{tag}."
+
+    if tag_record
+      query1 = Event
+        .join(:sites, id: :site_id)
+        .join(:sites_tags, site_id: :id)
+        .where(sites_tags__tag_id: tag_record.id)
+        .where(events__is_deleted: false, sites__is_deleted: false)
+        .where{sites__score > Event::ACTIVITY_TAG_SCORE_LIMIT}
+        .where(sites__is_nsfw: false)
+        .where(follow_id: nil)
+        .select_all(:events)
+
+      query2 = Event
+        .join(:sites, id: :actioning_site_id)
+        .join(:sites_tags, site_id: :id)
+        .where(sites_tags__tag_id: tag_record.id)
+        .where(events__is_deleted: false, sites__is_deleted: false)
+        .where{sites__score > Event::ACTIVITY_TAG_SCORE_LIMIT}
+        .where(sites__is_nsfw: false)
+        .where(follow_id: nil)
+        .select_all(:events)
+
+      if current_site
+        blocking_site_ids = current_site.blocking_site_ids
+        query1 = query1.where(Sequel.|({events__site_id: nil}, ~{events__site_id: blocking_site_ids})).where(Sequel.|({events__actioning_site_id: nil}, ~{events__actioning_site_id: blocking_site_ids}))
+        query2 = query2.where(Sequel.|({events__site_id: nil}, ~{events__site_id: blocking_site_ids})).where(Sequel.|({events__actioning_site_id: nil}, ~{events__actioning_site_id: blocking_site_ids}))
+      end
+
+      ds = query1.union(query2, all: false).order(Sequel.desc(:created_at))
+    else
+      ds = Event.where(id: nil).order(Sequel.desc(:created_at))
+    end
   else
     ds = Event.news_feed_default_dataset.exclude(sites__is_nsfw: true)
 
@@ -52,16 +72,20 @@ get '/activity' do
 
   record_count = nil
   if current_site.nil?
-    cache_key = if params[:tag]
-      ['activity_public_tag_record_count', params[:tag].to_s]
+    if params[:tag] && tag_record.nil?
+      record_count = 0
     else
-      :activity_public_record_count
-    end
+      cache_key = if params[:tag]
+        ['activity_public_tag_record_count', params[:tag]]
+      else
+        :activity_public_record_count
+      end
 
-    if SimpleCache.expired?(cache_key)
-      record_count = SimpleCache.store(cache_key, ds.count, 10.minutes)
-    else
-      record_count = SimpleCache.get(cache_key)
+      if SimpleCache.expired?(cache_key)
+        record_count = SimpleCache.store(cache_key, ds.count, 10.minutes)
+      else
+        record_count = SimpleCache.get(cache_key)
+      end
     end
 
     max_record_count = Event::GLOBAL_PAGINATION_LENGTH * 100
--- models/simple_cache.rb
+++ models/simple_cache.rb
@@ -2,24 +2,66 @@ require 'thread'
 require 'time'
 
 module SimpleCache
+  MAX_ENTRIES = 256
+
   @cache = {}
   @semaphore = Mutex.new
 
   class << self
     def store(name, value, timeout=30)
-      @semaphore.synchronize {
-        @cache[name] = {value: value, expires_at: Time.now+timeout}
-      }
+      @semaphore.synchronize do
+        now = Time.now
+        prune_expired(now)
+        @cache[name] = {value: value, expires_at: now+timeout, stored_at: now}
+        prune_oldest
+      end
       value
     end
 
     def get(name)
-      @cache[name][:value]
+      @semaphore.synchronize do
+        entry = @cache[name]
+        return nil if entry.nil?
+        if entry[:expires_at] <= Time.now
+          @cache.delete(name)
+          return nil
+        end
+        entry[:value]
+      end
     end
 
     def expired?(name)
-      return false if @cache[name] && @cache[name][:expires_at] > Time.now
-      true
+      @semaphore.synchronize do
+        entry = @cache[name]
+        return true if entry.nil?
+        return false if entry[:expires_at] > Time.now
+        @cache.delete(name)
+        true
+      end
+    end
+
+    def clear
+      @semaphore.synchronize { @cache.clear }
+    end
+
+    def size
+      @semaphore.synchronize { @cache.length }
+    end
+
+    private
+
+    def prune_expired(now=Time.now)
+      @cache.delete_if { |_name, entry| entry[:expires_at] <= now }
+    end
+
+    def prune_oldest
+      overflow = @cache.length - MAX_ENTRIES
+      return if overflow <= 0
+
+      @cache
+        .sort_by { |_name, entry| entry[:stored_at] }
+        .first(overflow)
+        .each { |name, _entry| @cache.delete(name) }
     end
   end
 end
